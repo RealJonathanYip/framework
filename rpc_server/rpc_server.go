2294f5ae45a108d67af816cc7178806f295664ce
yejianhui <yejianhui@bytedance.com>
2023-01-23 10:19:20 +0800

    update: http context logic...
    Change-Id: I30d4a0ea058a6818f756ca72728a0a66dee9dd0c

--- rpc_server/rpc_server.go
+++ rpc_server/rpc_server.go
@@ -3,12 +3,16 @@ package rpc_server
 import (
 	"context"
 	"fmt"
+	context0 "github.com/RealJonathanYip/framework/context0"
 	"github.com/RealJonathanYip/framework/interceptor"
 	"github.com/RealJonathanYip/framework/log"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/peer"
 	"net"
+	"strings"
+	"time"
 )
 
 type RpcServer struct {
@@ -19,12 +23,12 @@ type RpcServer struct {
 }
 
 func New(name string) *RpcServer {
-	return &RpcServer{
-		name: "rpc." + name,
-		server: grpc.NewServer(grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-			interceptor.WithServerTraceInterceptor(),
-		))),
-	}
+	rpcServer := &RpcServer{name: "rpc." + name}
+	rpcServer.server = grpc.NewServer(grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+		rpcServer.WithServerTraceInterceptor(),
+	)))
+
+	return rpcServer
 }
 
 func (r *RpcServer) Serve() {
@@ -47,7 +51,7 @@ func (r *RpcServer) Serve() {
 	}
 }
 
-func GetRpcServiceConnection(serviceName string) (*grpc.ClientConn, error) {
+func (r *RpcServer) GetRpcServiceConnection(serviceName string) (*grpc.ClientConn, error) {
 	//TODO: add service discover logic...
 	address := "127.0.0.1:8088"
 	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), interceptor.WithClientUnaryInterceptor())
@@ -61,4 +65,50 @@ func GetRpcServiceConnection(serviceName string) (*grpc.ClientConn, error) {
 	}
 
 	return conn, nil
+}
+
+// TODO：log and ip trace
+func (r *RpcServer) WithServerTraceInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		ctx = context0.FromRpcContext(ctx)
+
+		upstreamService, exist := context0.Get(ctx, context0.ContextKeyUpstreamService)
+		if !exist {
+			upstreamService = "unknow"
+		}
+
+		upstreamMethod, exist := context0.Get(ctx, context0.ContextKeyUpstreamMethod)
+		if !exist {
+			upstreamMethod = "unknow"
+		}
+
+		methodInfos := strings.Split(info.FullMethod, "/")
+		method := info.FullMethod
+		service := r.name
+		if len(methodInfos) == 3 {
+			method = methodInfos[2]
+			service = fmt.Sprintf("<grpc>-<%s>", methodInfos[1])
+		}
+
+		var upstreamAddress string
+		if peerTemp, ok := peer.FromContext(ctx); ok {
+			if tcpAddr, ok := peerTemp.Addr.(*net.TCPAddr); ok {
+				upstreamAddress = tcpAddr.String()
+			} else {
+				upstreamAddress = peerTemp.Addr.String()
+			}
+		}
+		context0.Set(ctx, context0.ContextKeyCurrentMethod, method,
+			context0.ContextKeyCurrentService, service,
+			context0.ContextKeyUpstreamAddress, upstreamAddress)
+
+		now := time.Now()
+		resp, err := handler(ctx, req)
+		cost := time.Since(now).Milliseconds()
+
+		log.Infof(ctx, "【serve】upstreamAddress:%s upstreamService:%v upstreamMethod:%v service:%v method:%v cost:%v(ms) req:%+v, resp:%+v",
+			upstreamAddress, upstreamService, upstreamMethod, service, method, cost, req, resp)
+
+		return resp, err
+	}
 }
